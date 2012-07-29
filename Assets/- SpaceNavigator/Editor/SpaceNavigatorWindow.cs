3726f64c47a28eb76d8a1353b2d995e9771ebb98
Patrick Hogenboom <patrick@fultonia.nl>
2012-07-29 18:52:48 +0200

    Improved motion model, always keep horizon horizontal. Azimuth is interpolated in world coordinates, pitch in local coordinates.
    ToDo: There is still some unexplained translation when the view is rotated.
    This is probably to do with how the SceneView is constructed from a camera, a pivot and a rotation.
    We can't move the camera as this is overwritten by unity.
    We can only set the pivot position and the rotation.

--- Assets/- SpaceNavigator/Editor/SpaceNavigatorWindow.cs
+++ Assets/- SpaceNavigator/Editor/SpaceNavigatorWindow.cs
@@ -4,9 +4,12 @@ using UnityEditor;
 
 public class SpaceNavigatorWindow : EditorWindow {
 	private bool _doViewportUpdate = true;
+	private bool _reset;
 
 	private const string TransSensKey = "Translation sensitivity";
 	private const string RotSensKey = "Rotation sensitivity";
+	private GameObject _dummyGO;
+	private Transform _dummy;
 
 	/// <summary>
 	/// Initializes the window.
@@ -18,6 +21,8 @@ public class SpaceNavigatorWindow : EditorWindow {
 		if (window) {
 			window.Show();
 			ReadSettings();
+			window._dummyGO = new GameObject("camera dummy");
+			window._dummy = window._dummyGO.transform;
 		}
 	}
 	/// <summary>
@@ -32,14 +37,64 @@ public class SpaceNavigatorWindow : EditorWindow {
 	/// </summary>
 	public void Update() {
 		SceneView sceneView = SceneView.lastActiveSceneView;
+
 		if (!sceneView) return;
+
 		
+		if (_reset) {
+			sceneView.pivot = new Vector3(0, 1, 0);
+			sceneView.rotation = Quaternion.identity;
+			sceneView.Repaint();
+		}
+
 		if (_doViewportUpdate && SpaceNavigator.Instance.HasNewData) {
-			Vector3 worldSpaceTranslation = SpaceNavigator.Instance.Translation.x*sceneView.camera.transform.right +
-			                                SpaceNavigator.Instance.Translation.y*sceneView.camera.transform.up +
-			                                SpaceNavigator.Instance.Translation.z*sceneView.camera.transform.forward;
-			sceneView.pivot += worldSpaceTranslation;
-			sceneView.rotation *= Quaternion.Euler(SpaceNavigator.Instance.Rotation);
+			Transform camera = sceneView.camera.transform;
+			_dummy.position = sceneView.pivot;
+			_dummy.rotation = sceneView.rotation;
+
+			// Perform translation.
+			_dummy.Translate(SpaceNavigator.Instance.Translation, Space.Self);
+
+			// Perform azimuth in world coordinates.
+			Vector3 euler = _dummy.rotation.eulerAngles;
+			euler.y += SpaceNavigator.Instance.Rotation.y;
+			_dummy.rotation = Quaternion.Euler(euler);
+
+			// Perform pitch in local coordinates.
+			Vector3 localEuler = _dummy.localRotation.eulerAngles;
+			localEuler.x += SpaceNavigator.Instance.Rotation.x;
+			_dummy.localRotation = Quaternion.Euler(localEuler);
+
+			sceneView.pivot = _dummy.position;
+			sceneView.rotation = _dummy.rotation;
+
+			//Vector3 worldSpaceTranslation = SpaceNavigator.Instance.Translation.x * sceneView.camera.transform.right +
+			//								SpaceNavigator.Instance.Translation.y * sceneView.camera.transform.up +
+			//								SpaceNavigator.Instance.Translation.z * sceneView.camera.transform.forward;
+			//sceneView.pivot += worldSpaceTranslation;
+
+			//Quaternion oldRot = sceneView.rotation;
+			//Quaternion azimuth = Quaternion.Euler(0, SpaceNavigator.Instance.Rotation.y, 0);
+			//Vector3 right = Vector3.right;
+			//right = oldRot * right;
+			//Quaternion pitch = Quaternion.AngleAxis(SpaceNavigator.Instance.Rotation.x, right);
+			//sceneView.rotation *= azimuth;
+			//sceneView.rotation *= pitch;
+
+			//// Move pivot by moving camera.
+			//Vector3 oldPos = camera.position;
+			//camera.Translate(SpaceNavigator.Instance.Translation, Space.Self);
+			//Vector3 deltaPos = camera.position - oldPos;
+			//sceneView.pivot += deltaPos;
+
+			//Quaternion oldRot = camera.rotation;
+			//camera.RotateAround(camera.position, Vector3.up, SpaceNavigator.Instance.Rotation.y);
+			////camera.RotateAroundLocal();
+			//Quaternion deltaRot = camera.rotation*Quaternion.Inverse(oldRot);
+			////sceneView.rotation *= deltaRot;
+
+			//sceneView.LookAtDirect(camera.position, Quaternion.identity);
+
 			sceneView.Repaint();
 		}
 	}
@@ -50,6 +105,7 @@ public class SpaceNavigatorWindow : EditorWindow {
 		GUILayout.BeginVertical();
 
 		_doViewportUpdate = GUILayout.Toggle(_doViewportUpdate, "SpaceNavigator viewport control");
+		_reset = GUILayout.Button("Reset");
 
 		GUILayout.BeginHorizontal();
 		GUILayout.Label(string.Format("T Sens {0:0.00000}", SpaceNavigator.Instance.TranslationSensitivity));
