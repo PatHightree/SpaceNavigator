f287ba3a1f0fb429fe1b302c3f85c16a2beac2b5
Patrick Hogenboom <patrick@fultonia.nl>
2013-03-23 00:48:19 +0100

    - Added horizon lock option to runtime access example script. - Added SmoothFollow2.cs

---
+++ Assets/SpaceNavigator/Scripts/SmoothFollow2.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class SmoothFollow2 : MonoBehaviour {
+	public Transform target;
+	public float distance = 3.0f;
+	public float height = 3.0f;
+	public float damping = 5.0f;
+	public bool smoothRotation = true;
+	public bool followBehind = true;
+	public float rotationDamping = 10.0f;
+
+	public void Update() {
+		Vector3 wantedPosition;
+		if (followBehind)
+			wantedPosition = target.TransformPoint(0, height, -distance);
+		else
+			wantedPosition = target.TransformPoint(0, height, distance);
+
+		transform.position = Vector3.Lerp(transform.position, wantedPosition, Time.deltaTime * damping);
+
+		if (smoothRotation) {
+			Quaternion wantedRotation = Quaternion.LookRotation(target.position - transform.position, target.up);
+			transform.rotation = Quaternion.Slerp(transform.rotation, wantedRotation, Time.deltaTime * rotationDamping);
+		} else transform.LookAt(target, target.up);
+	}
+}
