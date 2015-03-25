2b63c847b664b178d505b809dc5062ea7100babe
Patrick Hogenboom <patrick@fultonia.nl>
2015-03-25 12:54:49 +0100

    Added Unity 5 test project. No DllNotFoundException in 64 bit Unity editor.

---
+++ Space Navigator Unity project U5/Assets/Scripts/Test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class Test : MonoBehaviour {
+
+	[DllImport ("XCodePlugin")]
+	private static extern int InitPlugin(bool isDevice);
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (InitPlugin (false));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
