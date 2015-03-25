4c92174bdad128fbc77aec2fefffb7f78e26355a
Patrick Hogenboom <patrick@fultonia.nl>
2015-03-25 12:33:21 +0100

    Added missing Cocoa include. Added plugin code from this tutorial http://www.reigndesign.com/blog/unity-native-plugins-os-x/ Plugin project compiles. Added test script to Unity project. Test unsuccesful, DllNotFoundException: XCodePlugin

---
+++ Space Navigator Unity project/Assets/SpaceNavigator/Scripts/Test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class Test : MonoBehaviour {
+
+	[DllImport ("XCodePlugin")]
+	private static extern void InitPlugin(bool isDevice);
+
+	// Use this for initialization
+	void Start () {
+		InitPlugin (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- XCodePlugin/Plugin.m
+++ XCodePlugin/Plugin.m
@@ -7,9 +7,195 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
 #import "Plugin.h"
 #import "XCodePlugin_Prefix.pch"
 #import <3DConnexionClient/ConnexionClientAPI.h>
+
+MonoDomain *domain;
+NSString *assemblyPath;
+MonoAssembly *monoAssembly;
+MonoImage *monoImage;
+
+MonoMethodDesc *trackPadTouchBeganDesc;
+MonoMethod *trackPadTouchBeganMethod;
+
+MonoMethodDesc *trackPadTouchEndedDesc;
+MonoMethod *trackPadTouchEndedMethod;
+
+MonoMethodDesc *trackPadTouchCanceledDesc;
+MonoMethod *trackPadTouchCanceledMethod;
+
+NSView* view = nil;
+
+@interface TrackingObject : NSResponder
+{
+}
+- (void)touchesBeganWithEvent:(NSEvent *)event;
+- (void)touchesEndedWithEvent:(NSEvent *)event;
+- (void)touchesCancelledWithEvent:(NSEvent *)event;
+
+@end
+
+@implementation TrackingObject
+
+- (void)touchesBeganWithEvent:(NSEvent *)event
+{
+	float posX = 0.0f;
+	float posY = 0.0f;
+
+	NSSet *touches = [event touchesMatchingPhase:NSTouchPhaseBegan inView:view];
+	for (NSTouch *touch in touches)
+	{
+		posX = touch.normalizedPosition.x;
+		posY = touch.normalizedPosition.y;
+		break; //We just track one touch (no multi-touch in this example)
+	}
+
+	//Call method back
+	void *args[] = { &posX, &posY };
+	mono_runtime_invoke(trackPadTouchBeganMethod, NULL, args, NULL);
+}
+
+- (void)touchesEndedWithEvent:(NSEvent *)event
+{
+	float posX = 0.0f;
+	float posY = 0.0f;
+
+	NSSet *touches = [event touchesMatchingPhase:NSTouchPhaseBegan inView:view];
+	for (NSTouch *touch in touches)
+	{
+		posX = touch.normalizedPosition.x;
+		posY = touch.normalizedPosition.y;
+		break;
+	}
+
+	//Call method back
+	void *args[] = { &posX, &posY };
+	mono_runtime_invoke(trackPadTouchEndedMethod, NULL, args, NULL);
+}
+
+- (void)touchesCancelledWithEvent:(NSEvent *)event
+{
+	float posX = 0.0f;
+	float posY = 0.0f;
+
+	NSSet *touches = [event touchesMatchingPhase:NSTouchPhaseBegan inView:view];
+	for (NSTouch *touch in touches)
+	{
+		posX = touch.normalizedPosition.x;
+		posY = touch.normalizedPosition.y;
+		break;
+	}
+
+	//Call method back
+	void *args[] = { &posX, &posY };
+	mono_runtime_invoke(trackPadTouchCanceledMethod, NULL, args, NULL);
+}
+
+@end
+
+TrackingObject* pTrackMgr = nil;
+
+
+void SetupTrackingObject()
+{
+    NSLog(@"Native plugin -> SetupTrackingObject");
+    
+//    NSApplication* app = [NSApplication sharedApplication];
+//    NSWindow* window = [app mainWindow];
+//    view = [window contentView];
+//    
+//    if(pTrackMgr != nil)
+//    {
+//        [pTrackMgr release];
+//        pTrackMgr = nil;
+//    }
+//    
+//    pTrackMgr = [TrackingObject alloc];
+//    [view setAcceptsTouchEvents:YES];
+//    [view setNextResponder:pTrackMgr];
+    
+}
+
+void InitPlugin(bool isDevice)
+{
+	NSLog(@"Native plugin -> PluginInit - device? : %d", isDevice);
+
+//	SetupTrackingObject();
+//
+//	//We do this so it also works while we work on the Editor
+//	if(isDevice)
+//	{
+//		assemblyPath = [[[NSBundle mainBundle] bundlePath]
+//						stringByAppendingPathComponent:@"Contents/Data/Managed/Assembly-CSharp.dll"];
+//	}
+//	else
+//	{
+//		assemblyPath = @"[PATH_TO_YOUR_PROJECT]/Library/ScriptAssemblies/Assembly-CSharp.dll";
+//	}
+//
+//	NSLog(@"Native plugin -> assembly path: %@", assemblyPath);
+//
+//	domain = mono_domain_get();
+//	monoAssembly = mono_domain_assembly_open(domain, assemblyPath.UTF8String);
+//	monoImage = mono_assembly_get_image(monoAssembly);
+//
+//	//Note here that "MyNativePlugin" is the name of the unity class and "TrackPadTouchBegan" is the name of the static method we defined before.
+//	trackPadTouchBeganDesc = mono_method_desc_new("MyNativePlugin:TrackPadTouchBegan", FALSE);
+//	trackPadTouchBeganMethod = mono_method_desc_search_in_image(trackPadTouchBeganDesc, monoImage);
+//	mono_method_desc_free(trackPadTouchBeganDesc);
+//
+//	trackPadTouchEndedDesc = mono_method_desc_new("MyNativePlugin:TrackPadTouchEnded", FALSE);
+//	trackPadTouchEndedMethod = mono_method_desc_search_in_image(trackPadTouchEndedDesc, monoImage);
+//	mono_method_desc_free(trackPadTouchEndedDesc);
+//
+//	trackPadTouchCanceledDesc = mono_method_desc_new("MyNativePlugin:TrackPadTouchCanceled", FALSE);
+//	trackPadTouchCanceledMethod = mono_method_desc_search_in_image(trackPadTouchCanceledDesc, monoImage);
+//	mono_method_desc_free(trackPadTouchCanceledDesc);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //==============================================================================
