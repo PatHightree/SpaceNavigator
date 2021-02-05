82f37e42897cb852067223395aaf75861ed5bf75
Patrick Hogenboom <patrickhightree@gmail.com>
2021-02-05 23:10:49 +0100

    Added test project for the new input system

---
+++ SpaceNavigator Input system test/Assets/SpaceNavigator test/SpaceNavigatorTester.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+
+namespace UnityEngine.InputSystem.LowLevel
+{
+    public class SpaceNavigatorTester : MonoBehaviour
+    {
+        private SpaceNavigatorHID SN = new SpaceNavigatorHID();
+
+        private void Update()
+        {
+        }
+    }
+}
