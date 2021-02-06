4b9d276f09b5cfa1c7ed2fc4607f53d888075fe4
Patrick Hogenboom <patrickhightree@gmail.com>
2021-02-06 22:15:55 +0100

    Buttons are received (2)

--- SpaceNavigator Input system test/Assets/SpaceNavigator test/SpaceNavigatorHID.cs
+++ SpaceNavigator Input system test/Assets/SpaceNavigator test/SpaceNavigatorHID.cs
@@ -15,23 +15,33 @@ namespace UnityEngine.InputSystem.LowLevel
         public Vector3 rotation;
     } 
     
+    public struct ReportFormat3
+    {
+        public byte buttons;
+    } 
+    
     struct SpaceNavigatorHIDState : IInputStateTypeInfo
     {
         public FourCC format => new FourCC('H', 'I', 'D');
  
-        // In 1st report
+        // 1st report
         [InputControl(name = "rotation", format = "VC3S", layout = "Vector3", displayName = "Rotation")]
         [InputControl(name = "rotation/x", offset = 0, format = "SHRT", parameters = "scale=true, scaleFactor=1000")]
         [InputControl(name = "rotation/y", offset = 2, format = "SHRT", parameters = "scale=true, scaleFactor=1000")]
         [InputControl(name = "rotation/z", offset = 4, format = "SHRT", parameters = "scale=true, scaleFactor=1000")]
         public ReportFormat1 report1;
 
-        // In 2nd report
+        // 2nd report
         [InputControl(name = "translation", format = "VC3S", layout = "Vector3", displayName = "Translation")]
         [InputControl(name = "translation/x", offset = 0, format = "SHRT", parameters = "scale=true, scaleFactor=1000")]
         [InputControl(name = "translation/y", offset = 2, format = "SHRT", parameters = "scale=true, scaleFactor=1000")]
         [InputControl(name = "translation/z", offset = 4, format = "SHRT", parameters = "scale=true, scaleFactor=1000")]
         public ReportFormat1 report2;
+        
+        // 3rd report
+        [InputControl(name = "button1", bit = 0, format = "BIT", layout = "Button", displayName = "Button 1")]
+        [InputControl(name = "button2", bit = 1, format = "BIT", layout = "Button", displayName = "Button 2")]
+        public ReportFormat3 report3;
     }
  
     #if UNITY_EDITOR
@@ -84,6 +94,8 @@ namespace UnityEngine.InputSystem.LowLevel
                 UnsafeUtility.MemCpy(&newState.report1, reportStatePtr, sizeof(ReportFormat1));
             else if (reportId == 2)
                 UnsafeUtility.MemCpy(&newState.report2, reportStatePtr, sizeof(ReportFormat2));
+            else if (reportId == 3)
+                UnsafeUtility.MemCpy(&newState.report3, reportStatePtr, sizeof(ReportFormat3));
          
             // Apply the state change. Don't simply MemCpy over currentStatePtr as that will lead to various
             // malfunctions. The system needs to do the memcpy itself.
