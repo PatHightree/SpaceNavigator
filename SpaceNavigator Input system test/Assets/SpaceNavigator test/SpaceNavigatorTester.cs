49f55c79d41d7c206aab0af7d7871cbd2463cd3b
Patrick Hogenboom <patrickhightree@gmail.com>
2021-02-07 00:48:41 +0100

    Reading data from device in script works

--- SpaceNavigator Input system test/Assets/SpaceNavigator test/SpaceNavigatorTester.cs
+++ SpaceNavigator Input system test/Assets/SpaceNavigator test/SpaceNavigatorTester.cs
@@ -5,10 +5,12 @@ namespace UnityEngine.InputSystem.LowLevel
 {
     public class SpaceNavigatorTester : MonoBehaviour
     {
-        private SpaceNavigatorHID SN = new SpaceNavigatorHID();
-
         private void Update()
         {
+            transform.Translate(SpaceNavigatorHID.current.Translation.ReadValue(), Space.Self);
+            transform.Rotate(transform.right, SpaceNavigatorHID.current.Rotation.ReadValue().x, Space.Self);
+            transform.Rotate(Vector3.up, SpaceNavigatorHID.current.Rotation.ReadValue().y, Space.World);
+            transform.Rotate(transform.forward, SpaceNavigatorHID.current.Rotation.ReadValue().z, Space.Self);
         }
     }
 }
