88d4fc0e41278f7422461ebf871ebc95864b01fd
Patrick Hogenboom <patrickhightree@gmail.com>
2021-02-08 21:01:12 +0100

    Tuned sensitivity and added horizon lock in test scene

--- SpaceNavigator Input system test/Assets/SpaceNavigator test/SpaceNavigatorHID.cs
+++ SpaceNavigator Input system test/Assets/SpaceNavigator test/SpaceNavigatorHID.cs
@@ -27,16 +27,16 @@ namespace UnityEngine.InputSystem.LowLevel
 
         // 1st report
         [InputControl(name = "translation", format = "VC3S", layout = "Vector3", displayName = "Translation")] 
-        [InputControl(name = "translation/x", offset = 0, format = "SHRT", parameters = "scale=true, scaleFactor=1000")] 
-        [InputControl(name = "translation/y", offset = 4, format = "SHRT", parameters = "scale=true, scaleFactor=-1000")]
-        [InputControl(name = "translation/z", offset = 2, format = "SHRT", parameters = "scale=true, scaleFactor=-1000")]
+        [InputControl(name = "translation/x", offset = 0, format = "SHRT", parameters = "scale=true, scaleFactor=100")] 
+        [InputControl(name = "translation/y", offset = 4, format = "SHRT", parameters = "scale=true, scaleFactor=-100")]
+        [InputControl(name = "translation/z", offset = 2, format = "SHRT", parameters = "scale=true, scaleFactor=-100")]
         public ReportFormat1 report1;
 
         // 2nd report
         [InputControl(name = "rotation", format = "VC3S", layout = "Vector3", displayName = "Rotation")] 
-        [InputControl(name = "rotation/x", offset = 0, format = "SHRT", parameters = "scale=true, scaleFactor=-1000")] 
-        [InputControl(name = "rotation/y", offset = 4, format = "SHRT", parameters = "scale=true, scaleFactor=1000")] 
-        [InputControl(name = "rotation/z", offset = 2, format = "SHRT", parameters = "scale=true, scaleFactor=1000")]
+        [InputControl(name = "rotation/x", offset = 0, format = "SHRT", parameters = "scale=true, scaleFactor=-100")] 
+        [InputControl(name = "rotation/y", offset = 4, format = "SHRT", parameters = "scale=true, scaleFactor=100")] 
+        [InputControl(name = "rotation/z", offset = 2, format = "SHRT", parameters = "scale=true, scaleFactor=100")]
         public ReportFormat1 report2;
 
         // 3rd report
--- SpaceNavigator Input system test/Assets/SpaceNavigator test/SpaceNavigatorTester.cs
+++ SpaceNavigator Input system test/Assets/SpaceNavigator test/SpaceNavigatorTester.cs
@@ -5,12 +5,23 @@ namespace UnityEngine.InputSystem.LowLevel
 {
     public class SpaceNavigatorTester : MonoBehaviour
     {
+        public bool HorizonLock = true;
         private void Update()
         {
             transform.Translate(SpaceNavigatorHID.current.Translation.ReadValue(), Space.Self);
-            transform.Rotate(transform.right, SpaceNavigatorHID.current.Rotation.ReadValue().x, Space.Self);
-            transform.Rotate(Vector3.up, SpaceNavigatorHID.current.Rotation.ReadValue().y, Space.World);
-            transform.Rotate(transform.forward, SpaceNavigatorHID.current.Rotation.ReadValue().z, Space.Self);
+            
+            if (HorizonLock)
+            {
+                transform.Rotate(Vector3.up, SpaceNavigatorHID.current.Rotation.ReadValue().y, Space.World);
+                transform.Rotate(Vector3.right, SpaceNavigatorHID.current.Rotation.ReadValue().x, Space.Self);
+            }
+            else
+            {
+                transform.Rotate(Vector3.right, SpaceNavigatorHID.current.Rotation.ReadValue().x, Space.Self);
+                transform.Rotate(Vector3.up, SpaceNavigatorHID.current.Rotation.ReadValue().y, Space.Self);
+                transform.Rotate(Vector3.forward, SpaceNavigatorHID.current.Rotation.ReadValue().z, Space.Self);
+            }
+                
         }
     }
 }
