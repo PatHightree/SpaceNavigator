fd244d94ccca6b4d638d36e2eee2a418678c4bc8
Patrick Hogenboom <patrickhightree@gmail.com>
2021-03-14 16:45:51 +0100

    Excluded ViewportController editor-only code from build

--- Editor/ViewportController.cs
+++ Editor/ViewportController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if UNITY_EDITOR
+using System;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -377,3 +378,4 @@ namespace SpaceNavigatorDriver
         #endregion - Snapping -
     }
 }
+#endif
