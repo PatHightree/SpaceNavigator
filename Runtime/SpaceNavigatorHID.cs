33da2a8de740088984fb36e6bd627a7b918f3f70
cp <cp>
2022-02-11 21:26:56 +0100

    Complete refactor of everything device-related.
    Pulled in some internal functionality from InputSystem's HID classes to leverage the auto-config of HIDElementDescriptor.
    Latter works great in isolation but Unity's implementation is focussed on Gamepad and Joystick devices by nature. The auto-generated layout for space mice is not so great.
    It needed some work to craft a LayoutBuilder that accounts for the special needs of our beloved space mice.
    
    Layouts will be built correctly for all devices supplying a correct HID descriptor. In theory at least, needs testing with other devices.
    Reports now get merged into one big struct. Its size is chosen generously to account even for bigger report sizes. Needs to be reviewed.
    Element's offsets are calculated with respect to their reportId.
    Enable grouping of elements with consecutive GenericDesktop usage so we can form nice translation/rotation vectors.
    Some hacks are in place to overcome the auto-generated HID layout getting in the way, especially on editor startup.

--- Runtime/SpaceNavigatorHID.cs
+++ Runtime/SpaceNavigatorHID.cs
@@ -1,146 +1,63 @@
+using System;
+using System.Collections;
+using System.Linq;
+using System.Runtime.InteropServices;
 using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.InputSystem.Controls;
+using UnityEngine.InputSystem.HID;
 using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.InputSystem.Utilities;
-using System.Runtime.InteropServices;
 
 namespace SpaceNavigatorDriver
 {
-
-    [StructLayout(LayoutKind.Explicit)]
-    struct SpaceNavigatorHIDState : IInputStateTypeInfo
+    //[InputControlLayout(stateType = typeof(MergedReports))]
+    public class SpaceNavigatorHID : HID, IInputStateCallbackReceiver
     {
-        public FourCC format => new FourCC('H', 'I', 'D');
+        internal const int ReportSizeMax = 33;
+        internal const int ReportCountMax = 3;
+        internal const int StateSizeMax = ReportSizeMax * ReportCountMax;
 
-        [FieldOffset(0)]
-        public byte reportId;
+        [StructLayout(LayoutKind.Explicit, Size = StateSizeMax)]
+        public struct MergedReports
+        { }
 
-        // 1st report
-        // Normalize min/max values: Incoming values are in the range of [-350, 350], so we just use 350/Int16.MaxValue ~= 0.0106
-        [InputControl(name = "translation", format = "VC3S", layout = "Vector3", displayName = "Translation")]
-        [InputControl(name = "translation/x", offset = 0, format = "SHRT", parameters = "normalize=true,normalizeMin=-0.0106,normalizeMax=0.0106,normalizeZero=0.0, clamp=2,clampMin=-1,clampMax=1")]
-        [InputControl(name = "translation/y", offset = 4, format = "SHRT", parameters = "normalize=true,normalizeMin=-0.0106,normalizeMax=0.0106,normalizeZero=0.0, clamp=2,clampMin=-1,clampMax=1, invert=true")]
-        [InputControl(name = "translation/z", offset = 2, format = "SHRT", parameters = "normalize=true,normalizeMin=-0.0106,normalizeMax=0.0106,normalizeZero=0.0, clamp=2,clampMin=-1,clampMax=1, invert=true")]
-        [FieldOffset(1)]
-        public short translationX;
-        [FieldOffset(3)]
-        public short translationY;
-        [FieldOffset(5)]
-        public short translationZ;
+        public static SpaceNavigatorHID current { get; private set; }
 
-        // 2nd report (or 2nd part of 1st report)
-        [InputControl(name = "rotation", format = "VC3S", layout = "Vector3", displayName = "Rotation")]
-        [InputControl(name = "rotation/x", offset = 0, format = "SHRT", parameters = "normalize=true,normalizeMin=-0.0106,normalizeMax=0.0106,normalizeZero=0.0, clamp=2,clampMin=-1,clampMax=1, invert=true")]
-        [InputControl(name = "rotation/y", offset = 4, format = "SHRT", parameters = "normalize=true,normalizeMin=-0.0106,normalizeMax=0.0106,normalizeZero=0.0, clamp=2,clampMin=-1,clampMax=1")]
-        [InputControl(name = "rotation/z", offset = 2, format = "SHRT", parameters = "normalize=true,normalizeMin=-0.0106,normalizeMax=0.0106,normalizeZero=0.0, clamp=2,clampMin=-1,clampMax=1")]
-        [FieldOffset(7)]
-        public short rotationX;
-        [FieldOffset(9)]
-        public short rotationY;
-        [FieldOffset(11)]
-        public short rotationZ;
-
-        // 3rd report
-        [InputControl(name = "button1", bit = 0, format = "BIT", layout = "Button", displayName = "Button 1")]
-        [InputControl(name = "button2", bit = 1, format = "BIT", layout = "Button", displayName = "Button 2")]
-        [FieldOffset(13)]
-        public ButtonReport buttonsState;
-
-        [StructLayout(LayoutKind.Explicit)]
-        public struct ButtonReport
-        {
-            [FieldOffset(0)]
-            public byte reportId;
-
-            [FieldOffset(1)]
-            public bool button1;
-
-            [FieldOffset(2)]
-            public bool button2;
-        }
-    }
-
-#if UNITY_EDITOR
-    [UnityEditor.InitializeOnLoad] // Make sure static constructor is called during startup.
-#endif
-    [InputControlLayout(stateType = typeof(SpaceNavigatorHIDState))]
-    public class SpaceNavigatorHID : InputDevice, IInputStateCallbackReceiver
-    {
         public ButtonControl Button1 { get; protected set; }
         public ButtonControl Button2 { get; protected set; }
         public Vector3Control Rotation { get; protected set; }
         public Vector3Control Translation { get; protected set; }
-        
-        static SpaceNavigatorHID()
-        {
-#if !ENABLE_INPUT_SYSTEM
-            Debug.LogError("SpaceNavigator Driver cannot function because the <b>New Input System Package</b> is not active !\n" +
-                           "Please enable it in <i>Project Settings/Player/Active Input Handling</i>.");
-#endif
-            // If no layout with a matching product ID is found, this will be the default. 
-            InputSystem.RegisterLayout<SpaceNavigatorHID>(
-                matches: new InputDeviceMatcher()
-                    .WithInterface("HID")
-                    .WithManufacturer("3Dconnexion.*")
-            );
-            DebugLog("SpaceNavigatorHID : RegisterLayout");
-        }
 
-        // In the player, trigger the calling of our static constructor
-        // by having an empty method annotated with RuntimeInitializeOnLoadMethod.
-        [RuntimeInitializeOnLoadMethod]
-        static void Init()
-        {
-        }
-
-        // FinishSetup is where our device setup is finalized. Here we can look up
-        // the controls that have been created.
         protected override void FinishSetup()
         {
             base.FinishSetup();
 
-            displayName = GetType().Name;
-            Translation = GetChildControl<Vector3Control>("translation");
-            Rotation = GetChildControl<Vector3Control>("rotation");
             Button1 = GetChildControl<ButtonControl>("button1");
             Button2 = GetChildControl<ButtonControl>("button2");
-
-            DebugLog("SpaceNavigatorHID : FinishSetup");
+            Rotation = GetChildControl<Vector3Control>("rotation");
+            Translation = GetChildControl<Vector3Control>("translation");
         }
 
-        // We can also expose a '.current' getter equivalent to 'Gamepad.current'.
-        // Whenever our device receives input, MakeCurrent() is called. So we can
-        // simply update a '.current' getter based on that.
-        public static SpaceNavigatorHID current { get; private set; }
         public override void MakeCurrent()
         {
             base.MakeCurrent();
+
             if (current == this)
                 return;
-            current = this;
-            DebugLog("SpaceNavigatorHID : MakeCurrent");
-        }
 
-        // When one of our custom devices is removed, we want to make sure that if
-        // it is the '.current' device, we null out '.current'.
-        protected override void OnRemoved()
-        {
-            base.OnRemoved();
-            if (current == this)
-                current = null;
-            DebugLog("SpaceNavigatorHID : OnRemoved");
+            current = this;
+
+            DebugLog($"Current instance: {displayName}");
         }
 
         public void OnNextUpdate()
-        {
-        }
+        { }
 
-        public virtual unsafe void OnStateEvent(InputEventPtr eventPtr)
+        public unsafe void OnStateEvent(InputEventPtr eventPtr)
         {
-            // Refuse delta events.
             if (eventPtr.IsA<DeltaStateEvent>())
                 return;
 
@@ -148,60 +65,58 @@ namespace SpaceNavigatorDriver
             if (stateEventPtr->stateFormat != new FourCC('H', 'I', 'D'))
                 return;
 
-            var reportPtr = (byte*) stateEventPtr->state;
+            var reportPtr = (byte*)stateEventPtr->state;
             var reportId = *reportPtr;
-            var reportStatePtr = (reportPtr + 1); // or wherever the actual report starts.
 
-            // We have two options here. We can either use InputState.Change with a DeltaStateEvent that we set up
-            // from the event we have received (and simply update either report1 or report2 only) or we can merge
-            // our current state with the state we have just received. The latter is simpler so we do that here.
+            if (reportId < 1 || reportId > ReportCountMax)
+                return;
 
-            var newState = default(SpaceNavigatorHIDState);
-            // Can opt to only copy the state that we won't override. We don't bother here.
-            UnsafeUtility.MemCpy(&newState, (byte*)currentStatePtr + stateBlock.byteOffset, sizeof(SpaceNavigatorHIDState));
-            
-            if (reportId == 1)
-            {
-                var reportLength = stateEventPtr->stateSizeInBytes;
+            // Copy current state.
+            // Use valueSizeInBytes because the state block size is defined by the max element offset + its size.
+            // That means the state is _not_ the size we set in LayoutBuilder!
+            var newState = new MergedReports();
+            UnsafeUtility.MemCpy(&newState, (byte*)currentStatePtr + stateBlock.byteOffset, valueSizeInBytes);
 
-                //only contains translation
-                if (reportLength == 7)
-                    UnsafeUtility.MemCpy(&newState, reportPtr, 7);
-                // contains translation and rotation
-                else if (reportLength == 13)
-                    UnsafeUtility.MemCpy(&newState, reportPtr, 13);
+            // Merge incoming report into the current state
+            // Use reportId to map to a specific block inside the state struct
+            UnsafeUtility.MemCpy(((byte*)&newState) + ReportSizeMax * (reportId - 1), reportPtr, stateEventPtr->stateSizeInBytes);
+            //DebugLog($"Copied report {reportId} {stateEventPtr->stateSizeInBytes}:\n" + Hex(&newState, StateSizeMax, ReportSizeMax));
 
-                DebugLog("SpaceNavigatorHID : Copied report1");
-            }
-            else if (reportId == 2)
-            {
-                UnsafeUtility.MemCpy(((byte*)&newState) + 7, reportPtr + 1, 6);
-
-                DebugLog("SpaceNavigatorHID : Copied report2");
-            }
-            else if (reportId == 3)
-            {
-                UnsafeUtility.MemCpy(&newState.buttonsState, reportStatePtr, sizeof(SpaceNavigatorHIDState.ButtonReport));
-                DebugLog("SpaceNavigatorHID : Copied report3");
-            }
-
-            // Apply the state change. Don't simply MemCpy over currentStatePtr as that will lead to various
-            // malfunctions. The system needs to do the memcpy itself.
             InputState.Change(this, newState, eventPtr: eventPtr);
         }
 
         public bool GetStateOffsetForEvent(InputControl control, InputEventPtr eventPtr, ref uint offset)
         {
-            // If this isn't implemented, some stuff like auto-switching won't work correctly.
-            // If you want to implement this, return a state offset corresponding to the control in the given input event.
+            // TODO: How to implement this?
             return false;
         }
 
-        public static void DebugLog(string _message)
+        internal static void DebugLog(string msg)
         {
 #if SPACENAVIGATOR_DEBUG
-            Debug.Log(_message);
+            Debug.Log($"{nameof(SpaceNavigatorHID)}: {msg}");
 #endif
         }
+
+#if SPACENAVIGATOR_DEBUG
+        private unsafe string Hex(void* newState, int length, int stride = -1)
+        {
+            byte[] b = new byte[length];
+            fixed (byte* ptr = &b[0])
+            {
+                UnsafeUtility.MemCpy(ptr, newState, length);
+            }
+
+            if (stride < 0)
+                stride = length;
+
+            var hexString = string.Join(
+                "\n",
+                Enumerable.Range(0, length / stride)
+                    .Select(i => BitConverter.ToString(b.Skip(i * stride).Take(stride).ToArray()).Replace('-', ' ')));
+
+            return hexString;
+        }
+#endif
     }
 }
---
+++ Runtime/StringHelpers.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceNavigatorDriver
+{
+    // Pulled in InputSystem's internal StringHelpers.
+    // Removed unneeded parts.
+    internal static class StringHelpers
+    {
+        public static string Join<TValue>(string separator, params TValue[] values)
+        {
+            return Join(values, separator);
+        }
+
+        public static string Join<TValue>(IEnumerable<TValue> values, string separator)
+        {
+            // Optimize for there not being any values or only a single one
+            // that needs no concatenation.
+            var firstValue = default(string);
+            var valueCount = 0;
+            StringBuilder result = null;
+
+            foreach (var value in values)
+            {
+                if (value == null)
+                    continue;
+                var str = value.ToString();
+                if (string.IsNullOrEmpty(str))
+                    continue;
+
+                ++valueCount;
+                if (valueCount == 1)
+                {
+                    firstValue = str;
+                    continue;
+                }
+
+                if (valueCount == 2)
+                {
+                    result = new StringBuilder();
+                    result.Append(firstValue);
+                }
+
+                result.Append(separator);
+                result.Append(str);
+            }
+
+            if (valueCount == 0)
+                return null;
+            if (valueCount == 1)
+                return firstValue;
+
+            return result.ToString();
+        }
+
+        public static string MakeUniqueName<TExisting>(string baseName, IEnumerable<TExisting> existingSet,
+            Func<TExisting, string> getNameFunc)
+        {
+            if (getNameFunc == null)
+                throw new ArgumentNullException(nameof(getNameFunc));
+
+            if (existingSet == null)
+                return baseName;
+
+            var name = baseName;
+            var nameLowerCase = name.ToLower();
+            var nameIsUnique = false;
+            var namesTried = 1;
+
+            // If the name ends in digits, start counting from the given number.
+            if (baseName.Length > 0)
+            {
+                var lastDigit = baseName.Length;
+                while (lastDigit > 0 && char.IsDigit(baseName[lastDigit - 1]))
+                    --lastDigit;
+                if (lastDigit != baseName.Length)
+                {
+                    namesTried = int.Parse(baseName.Substring(lastDigit)) + 1;
+                    baseName = baseName.Substring(0, lastDigit);
+                }
+            }
+
+            // Find unique name.
+            while (!nameIsUnique)
+            {
+                nameIsUnique = true;
+                foreach (var existing in existingSet)
+                {
+                    var existingName = getNameFunc(existing);
+                    if (existingName.ToLower() == nameLowerCase)
+                    {
+                        name = $"{baseName}{namesTried}";
+                        nameLowerCase = name.ToLower();
+                        nameIsUnique = false;
+                        ++namesTried;
+                        break;
+                    }
+                }
+            }
+
+            return name;
+        }
+    }
+}
